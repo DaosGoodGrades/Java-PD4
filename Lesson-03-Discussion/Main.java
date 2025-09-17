444a8545829560e3e92ebbec2643d5f2bd27ed34
Dao <daobinx@nycstudents.net>
2025-09-17 14:07:48 +0000

    kjlkjn

---
+++ Lesson-03-Discussion/Main.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+
+      // This is how to print to the console 
+      // in Java.
+      System.out.println("Daobin Xu");
+      System.out.println("Daobin Xu, 42-30, Union Street, Flushing NY 11355");
+      System.out.println("Daobin Xu, 42-30 \n Union Street, Flushing NY 11355");
+    
+  }
+
+  
+}
