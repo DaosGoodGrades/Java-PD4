e5a72dda6ccb6802f6239cb266a7e2cc71aef664
Dao <daobinx@nycstudents.net>
2025-10-09 14:14:47 +0000

    I did this in one day is this correct

---
+++ Lesson-10-Activity/Main.java
@@ -0,0 +1,24 @@
+
+class Main {
+
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+	void init(){
+  
+   
+
+  }
+
+  double gpa(double GPA ){
+	if(GPA > 90){
+		return GPA*1.1;
+	}
+	else {
+		return GPA;
+	}
+
+  }
+  
+}
